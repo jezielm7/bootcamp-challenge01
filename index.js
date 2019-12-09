a61208bf277ec2809e010d49e816e89a4089fc0c
Jeziel Marques <jezielmarques7@gmail.com>
2019-12-09 17:50:58 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const projects = [];
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+
+});
+
+server.post('/projects/:id/tasks', (req, res) => {
+  const { id } = req.params;
+  const { tasks } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(tasks);
+
+  return res.json(project);
+
+});
+
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+server.listen(3333);
