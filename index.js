0e0fc420f3bb68d80579fecd41628e65597451a3
Jeziel Marques <jezielmarques7@gmail.com>
2019-12-10 01:16:44 -0300

    Add route Put and Delete

--- index.js
+++ index.js
@@ -37,4 +37,25 @@ server.get('/projects', (req, res) => {
   return res.json(projects);
 });
 
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+server.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id == id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+});
+
 server.listen(3333);
